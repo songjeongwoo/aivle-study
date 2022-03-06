bc2ed6c0ad1f4f4f2612d59702ba3cdeee0886b5
songjeongwoo <nice920525@naver.com>
2022-03-07 00:11:27 +0900

    22-03-06-송정우-기능개발

--- baekjoon-python/같이 풀어볼 문제/22-03-06-송정우-기능개발.py
+++ baekjoon-python/같이 풀어볼 문제/22-03-06-송정우-기능개발.py
@@ -7,15 +7,15 @@ def solution(progresses, speeds):
     
     while progresses:
         cnt = 0
-        if progresses[0] >= 100:
-            for _ in range(len(progresses)):
+        if progresses[0] >= 100:  # 첫번째가 100이상이면
+            for _ in range(len(progresses)):  # 전체 중 100이상인 개수 카운트
                 if progresses[0] >= 100:
                     progresses.popleft()
                     speeds.popleft()
                     cnt+=1
                 else:
                     break
-        else:
+        else:  # 첫번째가 100미만이면 전체 과정에 speed 더하기
             for j in range(100):
                 if j < len(progresses):
                     progresses[j] += speeds[j]
